fcb6c2d44ad2e079c9dd16810df26433af576378
Osher <Qazwsxc0@gmail.com>
2026-06-20 19:24:09 +0300

    >Moved assets to a seperate folder >Starting building the main scene >Spin wheel prototype

---
+++ Assets/Scripts/RewardSpin.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class RewardSpin : MonoBehaviour
+{
+    [SerializeField] GameObject[] prizes;
+    [SerializeField] Sprite[] prizeSprites;
+    private float spinSpeed;
+    public float speedAccel;
+    private float accelFactor;
+    private float spinTime;
+
+    private float tempSpeedAccel;
+    
+    [HideInInspector]public int closestPrizeIndex;
+    void Start()
+    {
+        List<int> prizeList = new List<int>();
+
+        for (int i = 0; i < prizeSprites.Length; i++)
+        {
+            prizeList.Add(i);
+        }
+
+        for (int i = 0; i < prizeSprites.Length; i++)
+        {
+            int selectedPrize = Random.Range(0, prizeList.Count);
+            prizes[i].GetComponent<Image>().sprite = prizeSprites[prizeList[selectedPrize]];
+            prizeList.RemoveAt(selectedPrize);
+        }
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spinTime = Random.Range(3,7f); //5
+            spinSpeed = 0;
+            speedAccel = Random.Range(5,20f);//10 //20
+            tempSpeedAccel = -speedAccel;
+            accelFactor = (speedAccel * 2) / spinTime;
+            //accelAccelerator = 0; //2
+        }
+        if (spinTime>0) //|| speedAccel<tempSpeedAccel
+        {
+            spinSpeed += speedAccel;
+            speedAccel -= accelFactor * Time.deltaTime; //5 //8
+
+            spinTime -= Time.deltaTime;
+            if (spinTime <= 0)
+            {
+                float closestPrizeLength = float.MaxValue;
+                for (int i = 0; i < prizes.Length; i++)
+                {
+                    if (Mathf.Abs(prizes[i].transform.localPosition.y) < closestPrizeLength)
+                    {
+                        closestPrizeIndex = i; //for later purposes
+                        closestPrizeLength = prizes[i].transform.localPosition.y;
+                    }
+                }
+                //after finding the closest, apply it for everyone to snap it to the middle
+                for (int i = 0; i < prizes.Length; i++)
+                {
+                    prizes[i].transform.localPosition -= Vector3.up * closestPrizeLength;
+                    if (prizes[i].transform.localPosition.y > 500)
+                    {
+                        int temp = (i - 1) % 5;
+                        if (temp == -1)
+                            temp = 4;
+                        prizes[i].transform.localPosition = new Vector3(0, prizes[temp].transform.localPosition.y - 256, 0);
+                    }
+                }
+                return;
+            }
+
+            for (int i=0;i<prizes.Length;i++)
+            {
+                //prizes[i].transform.position -= Vector3.up * spinSpeed * Time.deltaTime;
+                prizes[i].transform.localPosition = new Vector3 (0, prizes[i].transform.localPosition.y - (spinSpeed * Time.deltaTime),0);
+                if (prizes[i].transform.localPosition.y<-500)
+                {
+                    //prizes[i].transform.localPosition = prizes[(i + 1) % 5].transform.localPosition + Vector3.up * 256;
+                    prizes[i].transform.localPosition = new Vector3(0,prizes[(i + 1) % 5].transform.localPosition.y + 256,0);
+                }
+            }
+
+        }
+    }
+}
